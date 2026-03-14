0b663a78402f2352f6b5e3e191b2effe5108a4e5
tanishq231 <77448114+tanishq231@users.noreply.github.com>
2026-03-14 13:01:48 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -5,19 +5,19 @@ public class Pattern {
 
         for (int i = 1; i <= n; i++) {
 
-            // print leading spaces
+            // leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // print increasing numbers
+            // increasing numbers
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(j + " ");
             }
 
-            // print decreasing numbers
+            // decreasing numbers
             for (int j = i - 1; j >= 1; j--) {
-                System.out.print(j);
+                System.out.print(j + " ");
             }
 
             System.out.println();
